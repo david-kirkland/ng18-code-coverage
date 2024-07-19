50d658cda9db4f41bae748cd9a759109d0fbb680
David Kirkland <David.Kirkland@disney.com>
2024-07-19 10:29:47 -0400

    works = npm run e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'cypress';
+import registerCodeCoverageTasks from '@cypress/code-coverage/task';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:4200',
     setupNodeEvents(on, config) {
-      require('@cypress/code-coverage/task')(on, config);
+      registerCodeCoverageTasks(on, config);
       return config;
     },
   },
